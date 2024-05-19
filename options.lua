39996ea819def7a2c15eac66d1902e379a0b4d85
Mirumo <mysteryperson520@gmail.com>
2024-05-19 16:38:40 +0800

    Test

---
+++ options.lua
@@ -0,0 +1,21 @@
+
+local opt = vim.opt
+
+-- tabs and indent
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.autoindent = true
+opt.number = true
+opt.expandtab = true
+
+
+-- search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- style
+opt.cursorline = true
+
+-- clipborad
+opt.clipboard:append('unnamedplus')
+
---
+++ plugins.lua
@@ -0,0 +1,44 @@
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+
+require('lazy').setup({
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'catppuccin'
+    end
+  },
+ {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", 
+      "MunifTanjim/nui.nvim",
+      "3rd/image.nvim"
+    },
+    config = function()
+      require("neo-tree").setup({
+        window = {
+          width = 30
+        }
+      })
+
+      vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
+    end
+  }
+})
