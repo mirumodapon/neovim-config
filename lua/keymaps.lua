d0e494f77da23e3e6737aeb4c33a15517af90f09
Mirumo <mysteryperson520@gmail.com>
2024-05-23 13:52:15 +0800

    update

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,4 +5,5 @@ keymap.set('n', '<leader>t', '<cmd>Neotree last<CR>')
 keymap.set('n', '<leader>ll', '<cmd>Neotree left<CR>')
 keymap.set('n', '<leader>l', '<cmd>Neotree float<CR>')
 
+keymap.set('n', '<leader>g', '<cmd>Neogit<CR>')
 
